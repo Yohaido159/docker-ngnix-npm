cc4ae6a6319a8d71de658bbf1be7cb8d15eedc14
Yohai <yohaido159@gmail.com>
2020-08-18 12:24:45 +0300

    change branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ddsdfsdf33</p>
+        <p>I changed on feature branch</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           learn react3
         </a>
